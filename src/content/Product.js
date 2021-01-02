22fa2c1e3a4f0767ef83482dfbc589fbcf3e83f7
Nattarika-Cha <s5902041620113@email.kmutnb.ac.th>
2021-01-02 17:33:29 +0700

    Home

--- src/content/Product.js
+++ src/content/Product.js
@@ -1,16 +1,16 @@
 import React, { Component } from "react";
-// import Close from '../img/Close.png';
-// import { Row, Col } from 'antd';
+// import { BsFillGrid1X2Fill} from 'react-icons/bs';
+import { FaProductHunt } from "react-icons/fa";
+import { Row, Col, Input, Select, Button } from 'antd';
 import { Container } from 'react-bootstrap';
-// import '../css/Home.css';
+import { PrinterOutlined, AppstoreAddOutlined } from '@ant-design/icons';
+import '../css/Product.css';
 
-// import axios from 'axios';
-// import Cookies from 'universal-cookie';
+const { Option } = Select;
 
-// const cookies = new Cookies();
-
-// var ip = "http://localhost:5000";
-
+function handleChange(value) {
+  console.log(`selected ${value}`);
+}
 export default class Product extends Component {
     constructor(props) {
         super(props);
@@ -23,7 +23,34 @@ export default class Product extends Component {
     render() {
         return (
             <Container fluid>
-               
+               <Row id="product">
+                    <Col xs={1} md={1} xl={1} id="icon">
+                        <FaProductHunt style={{fontSize: '400%', color: '#DA213D' }}/>
+                    </Col>
+                    <Col xs={5} md={5} xl={5} id="page-product">
+                        สินค้า
+                    </Col>
+                </Row>
+                <Row id="input-search">
+                    <Input.Group compact>
+                        <Input.Search style={{ width: '30%' }} placeholder="ค้นหาสินค้า (รหัสสินค้า หรือ ชื่อสินค้า)" />
+                    </Input.Group>
+                </Row>
+                <Row id="input-search">
+                    <Col xs={2} md={2} xl={2}><div>เรียงลำดับตาม</div></Col>
+                    <Col xs={2} md={2} xl={2} id="col">
+                        <Select defaultValue="วันที่" style={{ width: 80 }} onChange={handleChange}>
+                            <Option value="jack">วันที่</Option>
+                            <Option value="lucy">ชื่อ</Option>
+                        </Select>
+                    </Col>
+                    <Col xs={4} md={4} xl={4} id="col">
+                        <Button id="button-print" icon={<PrinterOutlined />}>ปริ้นรายการสินค้า</Button>
+                    </Col>
+                    <Col xs={4} md={4} xl={4} id="col">
+                        <Button id="button-addproduct" icon={<AppstoreAddOutlined />}>เพิ่มรายการสินค้า</Button>
+                    </Col>
+                </Row>
             </Container>
         )
     }
--- src/template/Menu.js
+++ src/template/Menu.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'antd/dist/antd.css';
-
-import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
+import { BsFillGrid1X2Fill, BsFillPersonLinesFill, BsFillGearFill } from 'react-icons/bs';
+import { FaProductHunt } from "react-icons/fa";
 import { Menu } from 'antd';
 import { NavLink } from 'react-router-dom';
 
@@ -10,10 +10,10 @@ export default class MenuAdmin extends Component {
     render() {
         return (
             <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-                <Menu.Item key="1" icon={<UploadOutlined />}><NavLink to="/Home">หน้าหลัก</NavLink></Menu.Item>
-                <Menu.Item key="2" icon={<UserOutlined />}><NavLink to="/Product">สินค้า</NavLink></Menu.Item>
-                <Menu.Item key="3" icon={<VideoCameraOutlined />}><NavLink to="/User">สมาชิก</NavLink></Menu.Item>
-                <Menu.Item key="4" icon={<VideoCameraOutlined />}><NavLink to="/Setting">ตั้งค่า</NavLink></Menu.Item>
+                <Menu.Item key="1" icon={<BsFillGrid1X2Fill />}><NavLink to="/Home">หน้าหลัก</NavLink></Menu.Item>
+                <Menu.Item key="2" icon={<FaProductHunt />}><NavLink to="/Product">สินค้า</NavLink></Menu.Item>
+                <Menu.Item key="3" icon={<BsFillPersonLinesFill />}><NavLink to="/User">สมาชิก</NavLink></Menu.Item>
+                <Menu.Item key="4" icon={<BsFillGearFill />}><NavLink to="/Setting">ตั้งค่า</NavLink></Menu.Item>
             </Menu>
         );
     }
